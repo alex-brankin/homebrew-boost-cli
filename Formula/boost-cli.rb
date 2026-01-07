9c647766c81d21c92ac789ecc6fddda009f77899
Alex Brankin <73249808+alex-brankin@users.noreply.github.com>
2026-01-07 23:58:56 +0000

    Update formula to v1.0.1 - Add Boost mesh background to init screens

--- Formula/boost-cli.rb
+++ Formula/boost-cli.rb
@@ -1,22 +1,22 @@
 class BoostCli < Formula
   desc "CLI tool for syncing Boost Commerce templates with your local development environment"
   homepage "https://github.com/alex-brankin/boost-sync-cli"
-  version "1.0.0"
+  version "1.0.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/alex-brankin/homebrew-boost-cli/raw/main/releases/v1.0.0/boost-cli-macos-arm64"
-      sha256 "975875bab64fbab1490cf651136ec88eccda2d3c7a49cc83b8466ecd9ab568c1"
+      url "https://github.com/alex-brankin/homebrew-boost-cli/raw/main/releases/v1.0.1/boost-cli-macos-arm64"
+      sha256 "176e391002ec496059fd2c17230095bc2d084f7a431bbdcc666a653f2557455b"
     else
-      url "https://github.com/alex-brankin/homebrew-boost-cli/raw/main/releases/v1.0.0/boost-cli-macos-x64"
-      sha256 "136effdd1eecd411c6e62c847287515f1bf96e230046029007dd61dc913a684c"
+      url "https://github.com/alex-brankin/homebrew-boost-cli/raw/main/releases/v1.0.1/boost-cli-macos-x64"
+      sha256 "f898dcba740e763393afc46e29f2736777eafd14b1947befa6835633fa1e86a3"
     end
   end
 
   on_linux do
-    url "https://github.com/alex-brankin/homebrew-boost-cli/raw/main/releases/v1.0.0/boost-cli-linux-x64"
-    sha256 "580ec06a4f95444e0e7cd6fdb6ae08d336a8fe376dcba8e7fa66f5aa98aaab2a"
+    url "https://github.com/alex-brankin/homebrew-boost-cli/raw/main/releases/v1.0.1/boost-cli-linux-x64"
+    sha256 "80abe2337305b4fa35804b38aa37649a2168e41efc41b313ffc8ae4a04392e26"
   end
 
   def install
