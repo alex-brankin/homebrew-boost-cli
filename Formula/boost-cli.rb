36637ba58a43e93ff29428200ee6c6eaf738ecb2
Alex Brankin <73249808+alex-brankin@users.noreply.github.com>
2026-01-07 15:47:25 +0000

    Add v1.0.0 binaries and update formula to use homebrew-boost-cli repo

--- Formula/boost-cli.rb
+++ Formula/boost-cli.rb
@@ -6,16 +6,16 @@ class BoostCli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/alex-brankin/boost-sync-cli/releases/download/v1.0.0/boost-cli-macos-arm64"
+      url "https://github.com/alex-brankin/homebrew-boost-cli/raw/main/releases/v1.0.0/boost-cli-macos-arm64"
       sha256 "9bb53075fcd11a75215a8e2239dd87d97f7a617a05cac3b78e6f2a4219eed687"
     else
-      url "https://github.com/alex-brankin/boost-sync-cli/releases/download/v1.0.0/boost-cli-macos-x64"
+      url "https://github.com/alex-brankin/homebrew-boost-cli/raw/main/releases/v1.0.0/boost-cli-macos-x64"
       sha256 "2128471f2d98e1a944ce7c1dc48da10d966a9d53fead8410f9f99d7efc24e191"
     end
   end
 
   on_linux do
-    url "https://github.com/alex-brankin/boost-sync-cli/releases/download/v1.0.0/boost-cli-linux-x64"
+    url "https://github.com/alex-brankin/homebrew-boost-cli/raw/main/releases/v1.0.0/boost-cli-linux-x64"
     sha256 "4ad07f3e334db61f7e8528c0fa387f5343ffba5c03833ddc4f155c7d44a187de"
   end
 
